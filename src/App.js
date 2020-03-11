43a328de15466732c459702fc9658aba99c45d3f
Adria Castany <adria.castany@haufe.com>
2020-03-11 13:37:22 +0100

    Fully functional App :penguin:

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component{
-  render(){
-    return(
-      <div className="App">
-        <h1> Hello, Sales Admin! </h1>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="App">
+      <h1> Hello, Sales Admin! </h1>
+    </div>
+  );
+};
 
 export default App;
